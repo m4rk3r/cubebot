e98455229d5aa98575f3f5317208ba3a6e91734c
m4rk3r <mbeasl1@gmail.com>
2015-11-04 19:14:29 -0500

    Add delta for smooth scrolling

--- js/main.js
+++ js/main.js
@@ -2,30 +2,43 @@ var $w = $(window);
 var w = 50000;
 var h = 50000;
 var deltaX = 0;
+var lDeltaX = 0;
 var deltaY = 0;
+var lDeltaY = 0;
 
 $(function (){
 	window.scrollTo(w/2, h/2);
+	lDeltaX = w/2;
+	lDeltaY = h/2;
 	var timer;
 
 	var $cube = $('#cube');
 	var init_transform = 'translateX('+(window.innerWidth*0.5 + w*0.5-$cube.width()*0.5)+'px) '+
 						 'translateY('+(window.innerHeight*0.5 + h*0.5-$cube.height()*0.5)+'px)';
-	
 	//$cube.css('transform', init_transform);
+	
 	var suppress = false;
 
 	$(window).on('scroll', function (evt){ 
-		deltaX = ($w.scrollLeft()-w/2);
-		deltaY = ($w.scrollTop()-h/2);
+		if(!suppress){
+			var _x = ($w.scrollLeft()-w/2);
+			var _y = ($w.scrollTop()-h/2);
+			
+			deltaX +=  _x - lDeltaX;
+			deltaY +=  _y - lDeltaY;
 
-		// 'scale('+(1-deltaY *0.001)+','+(1-deltaY*0.001)+')
-		$cube.css('transform', 'translateY(-25px) translateZ(-500px) rotateX('+deltaY*0.1+'deg) rotateY('+deltaX*0.1+'deg)');
+			lDeltaX = _x; lDeltaY = _y;
 
-		console.log('x:',deltaX,'y:',deltaY);
+			$cube.css('transform', 'translateY(-25px) translateZ(-500px) rotateX('+deltaY*0.1+'deg) rotateY('+deltaX*0.1+'deg)');
+		}else{
+			suppress = false;
+			lDeltaX = 0;
+			lDeltaY = 0;
+		}
 
 		clearTimeout(timer);
 		timer = setTimeout(function (){
+			suppress = true;
 			console.log('resetting window position');
 			window.scrollTo(w/2, h/2);
 		},250);
