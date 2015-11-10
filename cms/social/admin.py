6b6c66a5d59d9025d328c366f41aad8f1a681100
m4rk3r <mbeasl1@gmail.com>
2015-11-09 21:39:26 -0500

    Move CMS to its own repo

--- cms/social/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from models import *
-
-admin.site.register(InstagramLike)
-admin.site.register(YoutubeLike)
--- cms/solutions/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from models import Solution
-from serializers import SolutionSerializer
-
-
-class SolutionViewSet(viewsets.ModelViewSet):
-    queryset = Solution.objects.published()
-    serializer_class = SolutionSerializer
